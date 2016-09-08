40bab868f75e05ad4ea790cd322e20646510cfaa
wshim7 <wgshim7@gmail.com>
2016-09-08 12:34:40 -0400

    initial commit
    created 3d character moves around shooting tennis balls

---
+++ Sample_ball/Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeColor : MonoBehaviour {
+    Renderer render;
+	// Use this for initialization
+	void Start () {
+        render = GetComponent<Renderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetKeyDown(KeyCode.G))
+        {
+            render.material.color = Color.green;
+        }
+	}
+}
